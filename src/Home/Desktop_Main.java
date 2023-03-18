60158cafd27f73dc382096fc7059fc44abd0b7aa
EmmanuelJessi <107475789+EmmanuelJessi@users.noreply.github.com>
2023-03-18 23:37:00 +0300

    Grid_panels

--- src/Home/Desktop_Main.java
+++ src/Home/Desktop_Main.java
@@ -1,9 +1,22 @@
 package Home;
 
+import javax.swing.*;
+
 public class Desktop_Main {
 
     public static void main(String[] args) {
-        new Border_layout();
-        new Grid_layout();
+//          new Cards_With_Panels();
+//        new Border_layout();
+//        new Grid_layout();
+//        new Grid_panels();
+//        new Card_Layout();
+//        new Building_Menus();
+//        new panel_buttons();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MyFrame().setVisible(true);
+            }
+        });
+
     }
 }
